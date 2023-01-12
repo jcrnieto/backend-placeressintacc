17ac3b5dbc53a8ce2cb55cb640c0155c8bb43fc9
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2023-01-12 20:33:55 -0300

    Health check

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,118 +1,121 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require("jsonwebtoken");
 
+//health check
+const healthCheck = async (req, res) => {
+  try {
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //agregar productos
 const getProduct = async (req, res) => {
-
-try{
-   const result = await prisma.product.findMany();
-   res.json(result);
-}catch(err){
-    console.log(err)
-}
-}
+  try {
+    const result = await prisma.product.findMany();
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //agregar productos
 const addProduct = async (req, res) => {
-        console.log(req.file);
-        const {firebaseUrl} = req.file ? req.file : "";
-        
-        try{
-            const {title, description, price} = req.body;
-            const priceInt = parseInt(price);
+  console.log(req.file);
+  const { firebaseUrl } = req.file ? req.file : "";
 
-             const result = await prisma.product.create({
-                 data: {
-                     title,
-                     image:firebaseUrl,
-                     description,
-                     price:priceInt
-                 },
-              })
-              console.log(result)
-              res.json(result);
-            }catch(err){
-                console.log(err)
-            }
-}
+  try {
+    const { title, description, price } = req.body;
+    const priceInt = parseInt(price);
+
+    const result = await prisma.product.create({
+      data: {
+        title,
+        image: firebaseUrl,
+        description,
+        price: priceInt,
+      },
+    });
+    console.log(result);
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //eliminar producto
 const deleteProduct = async (req, res) => {
-    
-    try{
-     const {id} = req.params;
-     console.log(id)
-     if(id){
-        const result = await prisma.product.delete({
-            where: {id: Number(id)}
-         })
-         res.json(result);
-     }else{
-        return res.status(401).json({ error: "no existe id" });
-     }
-    }catch(err){
-        console.log(err)
+  try {
+    const { id } = req.params;
+    console.log(id);
+    if (id) {
+      const result = await prisma.product.delete({
+        where: { id: Number(id) },
+      });
+      res.json(result);
+    } else {
+      return res.status(401).json({ error: "no existe id" });
     }
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //editar productos
 const editProduct = async (req, res) => {
-    try{
-    const {id} = req.params;
-    const {title, image, description, price} = req.body;
+  try {
+    const { id } = req.params;
+    const { title, image, description, price } = req.body;
     const result = await prisma.product.update({
-        where: {id: Number(id)},
-        data: {title, image, description, price}
-    })
+      where: { id: Number(id) },
+      data: { title, image, description, price },
+    });
     res.json(result);
-}catch(err){
-    console.log(err)
-}
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getId = async (req, res) => {
-     try{
-       //console.log(req.params)
-        const id =req.params.id;
-       // console.log(id)
-        const idParams = parseInt(id)
-        //console.log('idparams',idParams)
-       const result = await prisma.product.findUnique({
-            where: {
-                id:idParams,
-            },
-            
-        });
-       
-        res.json(result);
-       
-     }catch(err){
-        console.log(err)
-     }
-}
+  try {
+    //console.log(req.params)
+    const id = req.params.id;
+    // console.log(id)
+    const idParams = parseInt(id);
+    //console.log('idparams',idParams)
+    const result = await prisma.product.findUnique({
+      where: {
+        id: idParams,
+      },
+    });
+
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getName = async (req, res) => {
-    try{
-       const title = req.query.title;
-       
-       const result = await prisma.product.findMany({
-          where:{title: String(title)},
-       });
-       res.json(result);
-    }catch(err){
-        console.log(err)
-    }
-}
+  try {
+    const title = req.query.title;
 
-
+    const result = await prisma.product.findMany({
+      where: { title: String(title) },
+    });
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
-    getProduct,
-    addProduct,
-    deleteProduct,
-    editProduct,
-    getId,
-    getName
-}
+  getProduct,
+  addProduct,
+  deleteProduct,
+  editProduct,
+  getId,
+  getName,
+  healthCheck,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,56 @@
-const { Router } = require('express');
-const {getProduct, addProduct, deleteProduct, editProduct, getId, getName}= require('../controllers/product.controllers');
-const {addUser, deleteUser, getUser} = require('../controllers/users.controllers');
-const {login} = require('../controllers/login.controllers');
-const {checkOuth} = require('../controllers/checkOuth.controllers');
-const {uploadImage} = require('../services/firebase');
+const { Router } = require("express");
+const {
+  getProduct,
+  addProduct,
+  deleteProduct,
+  editProduct,
+  getId,
+  getName,
+  healthCheck,
+} = require("../controllers/product.controllers");
+const {
+  addUser,
+  deleteUser,
+  getUser,
+} = require("../controllers/users.controllers");
+const { login } = require("../controllers/login.controllers");
+const { checkOuth } = require("../controllers/checkOuth.controllers");
+const { uploadImage } = require("../services/firebase");
 
-
-const  multer   =  require ( 'multer' );
+const multer = require("multer");
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: 1024 * 1024
+  storage: multer.memoryStorage(),
+  limits: 1024 * 1024,
 });
 
 const router = Router();
 
+//health-check
+router.get("/healthCheck", healthCheck);
+
 //rutas de productos
-router.get('/getProduct', getProduct);
+router.get("/getProduct", getProduct);
 
-router.post('/addProduct',upload.single('image'), uploadImage, addProduct);
+router.post("/addProduct", upload.single("image"), uploadImage, addProduct);
 
-router.delete('/deleteProduct/:id', deleteProduct);
+router.delete("/deleteProduct/:id", deleteProduct);
 
-router.put('/editProduct/:id', editProduct);
+router.put("/editProduct/:id", editProduct);
 
-router.get('/getId/:id', getId);
+router.get("/getId/:id", getId);
 
-router.get('/getName', getName)
+router.get("/getName", getName);
 
 //rutas de usuarios
-router.post('/addUser', addUser);
+router.post("/addUser", addUser);
 
-router.delete('/deleteUser/:id', deleteUser);
+router.delete("/deleteUser/:id", deleteUser);
 
-router.get('/getUser', getUser);
-
+router.get("/getUser", getUser);
 
 //rutas de login
-router.post('/login', login);
+router.post("/login", login);
 
 //ruta de firebase
-
-
-
-
 
 module.exports = router;
