929e55bc27df88ecb3594be46212021a107bb53c
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2023-01-12 19:34:33 -0300

    ruta get user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./src/app');
 //const PORT = process.env.PORT || 3001;
-//console.log(process.env.PORT)
+console.log(process.env.PORT)
 
 server.listen(3001, () => {
     console.log('%s listening at 3001');
